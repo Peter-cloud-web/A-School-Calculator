55e4a39cc52a11e03713d263b94ab7677eaa8686
Peter-cloud-web <petergichia165@gmail.com>
2019-11-23 12:12:35 +0300

    Add identation and space

---
+++ School.js
@@ -0,0 +1,25 @@
+
+function add()
+{
+var a=document.getElementById("one").value;
+var b=document.getElementById("two").value;
+
+var c=a+b;
+document.getElementById("ans").value=+c;
+}
+function sub()
+{
+var a=document.getElementById("one").value;
+var b=document.getElementById("two").value;
+
+var c=a-b;
+document.getElementById("ans").value=+c;
+}
+function mul()
+{
+var a=document.getElementById("one").value;
+var b=document.getElementById("two").value;
+
+var c=a*b;
+document.getElementById("ans").value=+c;
+}
